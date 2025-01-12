d8734ac6b6cdb5e68801e806fed82b9baf1c933c
WayneKim92 <eunchan0817@gmail.com>
2025-01-12 17:30:18 +0900

    Initial Commit

---
+++ Quick Natural ScrollTests/Quick_Natural_ScrollTests.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_Natural_ScrollTests.swift
+//  Quick Natural ScrollTests
+//
+//  Created by 김은찬 on 1/12/25.
+//
+
+import Testing
+@testable import Quick_Natural_Scroll
+
+struct Quick_Natural_ScrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
