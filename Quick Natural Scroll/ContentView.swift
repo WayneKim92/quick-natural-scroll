2b5606976a88bcc0559f44c01fd6f9f09e4df10a
WayneKim92 <eunchan0817@gmail.com>
2025-01-12 10:01:14 +0900

    Initial Commit

---
+++ Quick Natural Scroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quick Natural Scroll
+//
+//  Created by 김은찬 on 1/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quick Natural Scroll/Quick_Natural_ScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_Natural_ScrollApp.swift
+//  Quick Natural Scroll
+//
+//  Created by 김은찬 on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Quick_Natural_ScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
